28a299a4a8b43f46d3780e66c89f90ebdaae6a6d
Ruth Brand <ruthbrandsoprano@gmail.com>
2020-02-08 11:38:40 -0700

    variable practice

--- playground.js
+++ playground.js
@@ -1,4 +1,4 @@
-var1 = 1;
+var1 = 1; // one way to store a variable
 var2 = 2;
 var3 = 3;
 var4 = 4
@@ -8,3 +8,17 @@ const added = var1 + var2
 const subtracted = var3 - var4
 
 finalResult = added * subtracted
+
+
+let message; // another way to declare a variable (its empty right now)
+message = 'Hello';
+
+alert(message); //shows the variable content
+
+
+let message = 'Hello!'; // declare variable and set a value to it
+
+alert(message);
+
+let user = 'John', age = 25, message = 'Hello'; // multiple variables on
+//one line 
