e945efeeb45bf557e1f104f27c353eef6448db48
Ruth Brand <ruthbrandsoprano@gmail.com>
2020-02-08 11:50:11 -0700

    reassign variable to have two variables store the same data

--- playground.js
+++ playground.js
@@ -1,47 +1,11 @@
-var1 = 1; // one way to store a variable
-var2 = 2;
-var3 = 3;
-var4 = 4
-
-const added = var1 + var2
-
-const subtracted = var3 - var4
-
-finalResult = added * subtracted
-
-
-let message; // another way to declare a variable (its empty right now)
-message = 'Hello';
-
-alert(message); //shows the variable content
-
-
-let message = 'Hello!'; // declare variable and set a value to it
-
-alert(message);
-
-let user = 'John';
-age = 25;
-message = 'Hello';
-// multiple variables on
-//several lines
-//
-let user = 'John',
-  age = 25,
-  message = 'Hello';
-// multiple variables on several lines in a different style
-//
-let user = 'John'
-  , age = 25
-  , message = 'Hello';
-// another style to do multiple variables, comma first
-//
-var message = 'Hello'; //an old school way to do variable declaration
+let hello = 'Hello world!';
 
 let message;
 
-message = 'Hello!';
+// copy 'Hello World' from hello into message
+//
+message = hello;
 
-message = 'World'; // value changed
-
-alert(message);
+// now two variables hold the same data
+alert(hello); //hello world
+alert(message); //hello world
